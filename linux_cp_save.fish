e906aa62f08dae939e78ebcf26ef46945f800923
Daniel Chin <daniel.chin@nyu.edu>
2025-01-18 05:07:09 +0400

    start tracking manhattan

--- linux_cp_save.fish
+++ linux_cp_save.fish
@@ -1,4 +1,7 @@
 #!/usr/bin/env fish
 
-rm -r './first code'
-unzip "/home/dan/.factorio/saves/first code.zip" -d .
+# rm -r './first code'
+# unzip "/home/dan/.factorio/saves/first code.zip" -d .
+
+# rm -r './manhattan'
+unzip "/home/dan/.factorio/saves/manhattan.zip" -d .
