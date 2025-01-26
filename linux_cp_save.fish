fed5ca90fe26d6d157079b381cb1e817d6484e18
Daniel Chin <daniel.chin@nyu.edu>
2025-01-27 00:32:31 +0400

    add space age

--- linux_cp_save.fish
+++ linux_cp_save.fish
@@ -3,5 +3,8 @@
 # rm -r './first code'
 # unzip "/home/dan/.factorio/saves/first code.zip" -d .
 
-rm -r './manhattan'
-unzip "/home/dan/.factorio/saves/manhattan.zip" -d .
+# rm -r './manhattan'
+# unzip "/home/dan/.factorio/saves/manhattan.zip" -d .
+
+rm -r './space_ager'
+unzip "/home/dan/.factorio/saves/space_ager.zip" -d .
